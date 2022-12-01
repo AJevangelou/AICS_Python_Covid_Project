edae85f639a8f0504d371fd684c84d45d2130df0
Alex Evangelou <johnyevag95@gmail.com>
2022-12-01 09:19:34 +0200

    1. Aggregated the data by Country, created a new DataFrame and CSV with : Countries/Mean Latitude/Mean Longitude/Total Cases/Total Death/Total Recovered 2. Printing the unique Countries 3. Reporting the time span of the given dataset

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import os
 
 import pandas as pd
+from pandas import DataFrame
+
+# -----------------------------------------------------------------------------------------------------------
+#                                        GLOBAL VARIABLES DEFINITION
+# -----------------------------------------------------------------------------------------------------------
 
 COVID_PATH = os.path.join("datasets", "covid")
-print(COVID_PATH)
 COVID_CONFIRMED = os.path.join("confirmed.csv")
-COVID_DEATHS = os.path.join("time_series_covid_19_deaths.csv")
-COVID_RECOVERED = os.path.join("time_series_covid_19_recovered.csv")
+COVID_DEATHS = os.path.join("deaths.csv")
+COVID_RECOVERED = os.path.join("recovered.csv")
 
+
+# -----------------------------------------------------------------------------------------------------------
+#                                          FUNCTION DEFINITIONS:
+# -----------------------------------------------------------------------------------------------------------
 
 def load_data(covid_data_path):
     csv_path = os.path.join(COVID_PATH, covid_data_path)
@@ -22,25 +30,102 @@ def report_attributes(data):
         print(data_col_series)
 
 
-def main():
-    df_covid_confirmed = load_data(COVID_CONFIRMED)
+def createNewDf(df_confirmed, df_deaths, df_recovered, df_by_countries):
+    # Create arrays to populate the new DataFrame for Pandas
+    countriesArray = []
+    meanLatitude = []
+    meanLongitude = []
+    totalCases = []
+    totalDeaths = []
+    totalRecovered = []
 
-    g = df_covid_confirmed.groupby("Country/Region")
+    # Group confirmed cases by Country and iterate through them
+    confirmed = df_confirmed.groupby("Country/Region")
 
-    for country, country_df in g:
-        print(country)
-        print(country_df)
+    for country, country_df in confirmed:
+        #print(country)
+        countriesArray.append(country)
 
         # Get average latitude for each country
-        print(country_df.loc[:, "Lat"].mean())
+        #print("Mean latitude is: " + str(country_df.loc[:, "Lat"].mean()))
+        meanLatitude.append(country_df.loc[:, "Lat"].mean())
 
-        # Get average longtitude for each country
-        print(country_df.loc[:, "Long"].mean())
+        # Get average longitude for each country
+        #print("Mean longitude is: " + str(country_df.loc[:, "Long"].mean()))
+        meanLongitude.append(country_df.loc[:, "Long"].mean())
 
-        # Sum total cases
-        country_total = country_df.iloc[:len(country_df.index)].sum(axis=1, skipna=True, numeric_only=True)
-        print(country_total.values)
+        # Sum total cases (keeping only numeric values)
+        country_total = country_df.iloc[:, 4:].sum(1, numeric_only=True)
+
+        # Check if we have to iterate through regions
+        if country_df.shape[0] > 1:
+            #print("Total cases for the country: " + str(country_total.values.sum()) + "\n")
+            totalCases.append(country_total.values.sum())
+        else:
+            #print("Total cases for the country: " + str(country_total.values) + "\n")
+            totalCases.append(country_total.values)
+
+    #Start populating a new DataFrame with rows and columns
+    df_by_countries["Country"] = countriesArray
+    df_by_countries["Mean Latitude"] = meanLatitude
+    df_by_countries["Mean Longitude"] = meanLongitude
+    df_by_countries["Total Cases"] = totalCases
+
+    # Group confirmed cases by Country and iterate through them
+    deaths = df_deaths.groupby("Country/Region")
+
+    # Iterate through countries to sum deaths
+    for country, country_df in deaths:
+        country_total_deaths = country_df.iloc[:, 4:].sum(1, numeric_only=True)
+
+        if country_df.shape[0] > 1:
+
+            #print("Total deaths for the country: " + str(country_total_deaths.values.sum()) + "\n")
+            df_by_countries["Total Deaths"] = country_total_deaths.values.sum()
+            totalDeaths.append(country_total_deaths.values.sum())
+
+        else:
+            #print("Total deaths for the country: " + str(country_total_deaths.values))
+            totalDeaths.append(country_total_deaths.values)
+
+    # Populate the new DataFrame with Total Deaths
+    df_by_countries["Total Deaths"] = totalDeaths
+
+    # Group recovered cases by Country and iterate through them
+    recovered = df_recovered.groupby("Country")
+    for country, country_df in recovered:
+        country_total_recovered = country_df.iloc[:, 4:].sum(1, numeric_only=True)
+        if country_df.shape[0] > 1:
+            #print("Total recovered for the country: " + str(country_total_recovered.values.sum()) + "\n")
+            df_by_countries["Total Recovered"] = country_total_recovered.values.sum()
+            totalRecovered.append(country_total_recovered.values.sum())
+        else:
+            #print("Total recovered for the country: " + str(country_total_recovered.values))
+            totalRecovered.append(country_total_recovered.values)
+    # Populate the new DataFrame with Total Recovered
+    df_by_countries["Total Recovered"] = totalRecovered
+    print(countriesArray)
+    return df_by_countries
+
+def getDates(dataFrame):
+    print("The time span of the given dataset is between those dates: " + str(dataFrame.columns.values[4]) + " and " + str(dataFrame.columns.values[-1]))
 
 
-if __name__ == '__main__':
+def main():
+    #Load all data from the .csv
+    df_covid_confirmed = load_data(COVID_CONFIRMED)
+    df_covid_deaths = load_data(COVID_DEATHS)
+    df_covid_recovered = load_data(COVID_RECOVERED)
+
+    # Create a new DataFrame to store the new values
+    df_by_countries: DataFrame = pd.DataFrame()
+    updatedDF = createNewDf(df_covid_confirmed, df_covid_deaths, df_covid_recovered, df_by_countries)
+
+    # Save the new DataFrame to a .csv File
+    updatedDF.to_csv('newDF')
+
+    # Show the time span of the dataset
+    getDates(df_covid_confirmed)
+
+if __name__ == "__main__":
     main()
