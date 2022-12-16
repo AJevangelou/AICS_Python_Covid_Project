dc47487984e8611bdfa02e8261f02b7e9ed88be4
Alex Evangelou <johnyevag95@gmail.com>
2022-12-16 20:11:38 +0200

    Pending: develop an algorithm that can detect the number of distinct covid waves and their duration for each one of the top 10 Countries/Regions.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from pandas import DataFrame
 import matplotlib as mpl
 import geopandas
 import matplotlib.pyplot as plt
+from PIL import Image
+import glob
 
 # -----------------------------------------------------------------------------------------------------------
 #                                        GLOBAL VARIABLES DEFINITION
@@ -15,6 +17,7 @@ COVID_PATH = os.path.join("datasets", "covid")
 COVID_CONFIRMED = os.path.join("confirmed.csv")
 COVID_DEATHS = os.path.join("deaths.csv")
 COVID_RECOVERED = os.path.join("recovered.csv")
+COUNTRIES = []
 
 
 # -----------------------------------------------------------------------------------------------------------
@@ -86,6 +89,52 @@ def plotToMap(dataFrame, typeOfData, date, divisionConstant):
     plt.show()
 
 
+def plotToMapThreshold(dataFrame, typeOfData, date, divisionConstant, threshold, description):
+    # df_geo = dataFrame.GeoDataFrame(dataFrame, geometry=geopandas.points_from_xy(dataFrame["Lat"], dataFrame["Long"]))
+
+    # From GeoPandas, our world map data
+    worldmap = geopandas.read_file(geopandas.datasets.get_path("naturalearth_lowres"))
+    # Creating axes and plotting world map
+    fig, ax = plt.subplots(figsize=(12, 6))
+    worldmap.plot(color="lightgrey", ax=ax)
+
+    plt.title("COVID-19 {} on {} \nCountries with 100+ cases are represented with red".format(typeOfData, date))
+    plt.xlabel("Longitude")
+    plt.ylabel("Latitude")
+    # Creating axis limits and title
+    plt.xlim([-180, 180])
+    plt.ylim([-90, 90])
+
+    # Filter (drop) the countries with more or equal than 100 cases
+    lessData = dataFrame.drop(dataFrame.index[dataFrame[date] < threshold])
+
+    dataFrame.reset_index()
+    # Get sizes for the figure
+
+    # Filter (drop) the countries with less than 100 cases
+    dataFrame = dataFrame.drop(dataFrame.index[dataFrame[date] >= threshold])
+
+    size = dataFrame[date]
+
+    # Divide by a number, because size of circles would be too big to show
+    lessSize = lessData[date].div(divisionConstant)
+
+    # Add scatter plots to figure. Red for >= cases
+    plt.scatter(dataFrame["Long"], dataFrame["Lat"], s=size, color='blue', edgecolors="black", alpha=0.5)
+    plt.scatter(lessData["Long"], lessData["Lat"], s=lessSize, color='red', edgecolors="black", alpha=0.5)
+
+    plt.tight_layout()
+    plt.grid()
+
+    # Saving the figure
+    path = os.path.join("./datasets/figures/", typeOfData.replace(" ", ""))
+    os.makedirs(path, exist_ok=True)
+    plt.savefig("./datasets/figures/{}/".format(typeOfData.replace(" ", "")) + typeOfData + description)
+
+    # Displaying figure
+    plt.show()
+
+
 # Get total cases
 def sumCases(dataframe, typeOfdata):
     updatedDF: DataFrame
@@ -95,8 +144,9 @@ def sumCases(dataframe, typeOfdata):
 
 
 # Get top 10 countries by total cases
-def topCases(dataframe, typeOfdata):
+def topCases(dataframe, typeOfdata, description):
     topDF = dataframe.nlargest(10, typeOfdata)
+    COUNTRIES = topDF.index.tolist()
 
     # Figure Size
     fig, ax = plt.subplots(figsize=(10, 5))
@@ -126,7 +176,6 @@ def topCases(dataframe, typeOfdata):
     for i in ax.patches:
         plt.text(i.get_width() + 0.2, i.get_y() + 0.5, str(round((i.get_width()), 2)), fontsize=10, color='grey')
 
-
     # Add Plot Title
     ax.set_title('COVID-19 {} as of 29-May-21'.format(typeOfdata), loc='center')
     # Add x label
@@ -134,7 +183,72 @@ def topCases(dataframe, typeOfdata):
 
     # Show Plot
     plt.show()
+    createHistogram(dataframe, COUNTRIES, description)
 
+
+# Create a gif from saved figures
+def createGif():
+    # Build GIF
+    # Create the frames
+    frames = []
+    imgs = glob.glob("./datasets/figures/*.png")
+    for i in imgs:
+        new_frame = Image.open(i)
+        frames.append(new_frame)
+
+    # Save the png images into a GIF file that loops forever
+    frames[0].save('png_to_gif.gif', format='GIF',
+                   append_images=frames[1:],
+                   save_all=True,
+                   duration=300, loop=0)
+
+
+def createHistogram(dataFrame, countries_array, description):
+    # Get all dates from pandas
+    list_of_dates = dataFrame.columns.values.tolist()[2:]
+
+    len_of_dates = len(list_of_dates)
+
+    # print(dataFrame.loc[countries_array[0]][2:].values)
+    countries_data = {}
+    new_cases = [[], [], [], [], [], [], [], [], [], []]
+    for i in range(0, len(countries_array)):
+        list_of_values = dataFrame.loc[countries_array[i]][2:].values
+        print(countries_array[i])
+
+        new_cases[i].append(list_of_values[0])
+        for j in range(1, len(list_of_values)):
+            x = list_of_values[j] - list_of_values[j - 1]
+            if x < 0:
+                new_cases[i].append(0)
+            else:
+                new_cases[i].append(x)
+
+    fig, axes = plt.subplots(3, 4, figsize=(12, 8))
+    plt.subplots_adjust(hspace=0.5)
+    fig.suptitle("COVID-19 {} - Top 10 Country/Region".format(description))
+    fig.supxlabel("As of 29-May-2021")
+    fig.supylabel("New Cases")
+
+    #Iterate through top 10 countries and add sublplots to figures
+    index = 0
+    while index < 10:
+        for row in range(0, 3):
+            for column in range(0, 4):
+                if index >= 10:
+                    break
+                else:
+                    axes[row, column].plot(list_of_dates, new_cases[index])
+                    axes[row, column].set_title(countries_array[index])
+                    axes[row, column].set_xticks([0, int(len_of_dates / 3), int(2*len_of_dates/3), int(len_of_dates)])
+                    axes[row, column].xaxis.set_ticklabels(["Jan 20", "Jul 20", "Jan 21", "Jul 21"])
+                    index += 1
+
+    fig.delaxes(axes[2][2])
+    fig.delaxes(axes[2][3])
+
+    plt.tight_layout()
+    plt.show()
 
 def main():
     # Load all data from the .csv
@@ -156,34 +270,28 @@ def main():
     report_countries(updated_confirmed, "Country/Region")
     report_date(updated_confirmed)
 
-    # Plotting confirmed cases
-    plotToMap(updated_confirmed, "Confirmed Cases", "1/22/20", 1)
-    plotToMap(updated_confirmed, "Confirmed Cases", "5/28/21", 100000)
+    # Plotting confirmed cases with threshold
+    plotToMapThreshold(updated_confirmed, "Confirmed Cases", "1/22/20", 1, 100, "2")
+    plotToMapThreshold(updated_confirmed, "Confirmed Cases", "5/29/21", 100000, 100, "1")
 
     # Plotting recovered
-    plotToMap(updated_recovered, "Recovered", "1/22/20", 1)
-    plotToMap(updated_recovered, "Recovered", "5/28/21", 10000)
+    plotToMapThreshold(updated_recovered, "Recovered", "1/22/20", 1, 100, "2")
+    plotToMapThreshold(updated_recovered, "Recovered", "5/29/21", 10000, 100, "1")
 
     # Plotting Deaths
-    plotToMap(updated_deaths, "Deaths", "1/22/20", 1)
-    plotToMap(updated_deaths, "Deaths", "5/28/21", 1000)
-
-    # Sum cases and save updated CSV files
-    updated_confirmed = sumCases(updated_confirmed, "Total Confirmed")
-    updated_confirmed.to_csv('updated_confirmed.csv')
-
-    updated_recovered = sumCases(updated_recovered, "Total Recovered")
-    updated_recovered.to_csv('updated_recovered.csv')
-
-    updated_deaths = sumCases(updated_deaths, "Total Deaths")
-    updated_deaths.to_csv('updated_deaths.csv')
+    plotToMapThreshold(updated_deaths, "Deaths", "1/22/20", 1, 100, "2")
+    plotToMapThreshold(updated_deaths, "Deaths", "5/29/21", 1000, 100, "1")
 
     # Get top 10 countries by total cases
-    topCases(updated_confirmed, "Total Confirmed")
-    #Get top 10 countries for recovered cases
-    topCases(updated_recovered, "Total Recovered")
+    topCases(updated_confirmed, "5/29/21", "Confirmed Cases")
+    # Get top 10 countries for recovered cases
+    topCases(updated_recovered, "5/29/21", "Recovered Cases")
     # Get top 10 countries for deaths
-    topCases(updated_deaths, "Total Deaths")
+    topCases(updated_deaths, "5/29/21", "Deaths")
+
+    # Create an animated GIF with
+    createGif()
+
 
 if __name__ == "__main__":
     main()
